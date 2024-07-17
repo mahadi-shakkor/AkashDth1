05fe5064dc0bb58a051ac212c3ea81bc1947342d
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-17 10:26:28 +0600

    add tec controller class

--- src/main/java/com/oop/mahadi/akashdth1/TecnachianCanSeeServiceReqGoalControllers/oneView.java
+++ src/main/java/com/oop/mahadi/akashdth1/TecnachianCanSeeServiceReqGoalControllers/oneView.java
@@ -1,4 +1,4 @@
-package com.oop.mahadi.akashdth1.tecnachianCintrollerReqVire;
+package com.oop.mahadi.akashdth1.TecnachianCanSeeServiceReqGoalControllers;
 
 public class oneView
 {
---
+++ src/main/java/com/oop/mahadi/akashdth1/TecnachianGeneralController/TecnachianDashboardViewController.java
@@ -0,0 +1,66 @@
+package com.oop.mahadi.akashdth1.TecnachianGeneralController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class TecnachianDashboardViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void CustomerProfileButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DocumentsButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void FeedbackButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GraphButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RatingButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void applicationButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void submitdetailsButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void workReqButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
