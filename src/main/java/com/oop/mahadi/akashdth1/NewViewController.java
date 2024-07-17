8583180ecd5ea152ab14a5b393e04f4ecdcf768a
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-17 06:50:26 +0600

    add new view Fxml

---
+++ src/main/java/com/oop/mahadi/akashdth1/NewViewController.java
@@ -0,0 +1,20 @@
+package com.oop.mahadi.akashdth1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class NewViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
