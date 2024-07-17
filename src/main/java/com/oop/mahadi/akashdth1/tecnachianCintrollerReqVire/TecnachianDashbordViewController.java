99d84471648ff369d561b2433574cea9331f79ef
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-17 11:15:43 +0600

    add i tec dash view class

--- src/main/java/com/oop/mahadi/akashdth1/tecnachianCintrollerReqVire/TecnachianDashbordViewController.java
+++ src/main/java/com/oop/mahadi/akashdth1/tecnachianCintrollerReqVire/TecnachianDashbordViewController.java
@@ -2,6 +2,7 @@ package com.oop.mahadi.akashdth1.tecnachianCintrollerReqVire;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -14,10 +15,49 @@ public class TecnachianDashbordViewController {
     private URL location;
 
     @FXML
-    private Label lafxid;
+    private Label idFxidLavelShow;
 
- @javafx.fxml.FXML
-    public void initialize() {
+    @FXML
+    private Label nameFxidLavelShow;
+
+    @FXML
+    void CustomerProfileButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void FeedbackButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RatingButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addRow(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void submitdetailsButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void workReqButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
     }
 
 }
