8f36b573b2e480f49cda4dfca351599ce28871b4
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-17 08:24:45 +0600

    add tecnachian view Sceen

---
+++ src/main/java/com/oop/mahadi/akashdth1/TecnachianViewCustomerProfileContoller/TecnachianViewProfileCustomerContoller.java
@@ -0,0 +1,7 @@
+package com.oop.mahadi.akashdth1.TecnachianViewCustomerProfileContoller;
+
+public class TecnachianViewProfileCustomerContoller
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
