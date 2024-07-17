428ee25b25af7368d7c576eb87c50aa758447975
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-17 07:30:23 +0600

    Add mt tecnachian user Can buy pacage

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,6 @@ module com.oop.mahadi.akashdth1 {
     exports com.oop.mahadi.akashdth1;
     exports com.oop.mahadi.akashdth1.tecnachianCintrollerReqVire;
     opens com.oop.mahadi.akashdth1.tecnachianCintrollerReqVire to javafx.fxml;
+    exports com.oop.mahadi.akashdth1.CoustomerBuyPacageCcontroller;
+    opens com.oop.mahadi.akashdth1.CoustomerBuyPacageCcontroller to javafx.fxml;
 }
