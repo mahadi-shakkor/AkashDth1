9a8e7249a23b8f2a3a6d35a8b518230ed3d5e4bd
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-17 13:03:58 +0600

    Revert "name chande pkage"
    This reverts commit 13087ed4ba20e045b404700f7d3b652694253537.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,8 @@ module com.oop.mahadi.akashdth1 {
 
     opens com.oop.mahadi.akashdth1 to javafx.fxml;
     exports com.oop.mahadi.akashdth1;
-    exports com.oop.mahadi.akashdth1.TecnachianDashBoaedController;
-    opens com.oop.mahadi.akashdth1.TecnachianDashBoaedController to javafx.fxml;
+    exports com.oop.mahadi.akashdth1.tecnachianCintrollerReqVire;
+    opens com.oop.mahadi.akashdth1.tecnachianCintrollerReqVire to javafx.fxml;
     exports com.oop.mahadi.akashdth1.CoustomerBuyPacageCcontroller;
     opens com.oop.mahadi.akashdth1.CoustomerBuyPacageCcontroller to javafx.fxml;
 
