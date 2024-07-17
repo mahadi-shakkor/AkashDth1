7ed13a32c62cf00c1994ac6c28b1bc712ef32add
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-07-17 08:08:13 +0600

    Revert "Merge pull request #5 from mahadi-shakkor/newb2"
    This reverts commit 90b880a4c6e0fdf044349c454d791047cb226e5f, reversing
    changes made to 048d27e06714ae1ae2c060299db5edf1ae1ec750.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,4 @@ module com.oop.mahadi.akashdth1 {
     opens com.oop.mahadi.akashdth1.tecnachianCintrollerReqVire to javafx.fxml;
     exports com.oop.mahadi.akashdth1.CoustomerBuyPacageCcontroller;
     opens com.oop.mahadi.akashdth1.CoustomerBuyPacageCcontroller to javafx.fxml;
-    exports com.oop.mahadi.akashdth1.p;
-    opens com.oop.mahadi.akashdth1.p to javafx.fxml;
 }
